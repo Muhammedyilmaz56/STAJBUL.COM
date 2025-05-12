178d73051d204aac8872ab2a5b884f5a0349b7ae
Muhammedyilmaz56 <147645206+Muhammedyilmaz56@users.noreply.github.com>
2025-05-12 10:06:20 +0300

    İkinci commit: Modeller oluşturuldu

---
+++ app/Models/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    protected $fillable = ['student_id', 'internship_posting_id', 'cover_letter', 'status'];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function internshipPosting()
+    {
+        return $this->belongsTo(InternshipPosting::class);
+    }
+}
+
